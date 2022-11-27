2ec4b5f2ea89c94c1146726297cbd80a76da910e
Kikih Muktianing <kriehmukti@gmail.com>
2022-11-27 09:54:00 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World !<br>";
+
+$nama = "Aliyul";
+$umur = 23;
+
+echo "nama saya <strong>$nama</strong>, umur saya $umur tahun <br>";
+
+$namaAdik = "Iyul";
+$umurAdik = 15;
+
+echo "nama saya <strong>$namaAdik</strong>, umur saya $umurAdik tahun. <br>";
+
+$selisihumur = $umur - $umurAdik;
+
+echo "selisih usia saya dan adik saya adalah $selisihumur tahun";
+
+?>
